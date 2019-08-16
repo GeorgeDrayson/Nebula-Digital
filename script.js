bd1ecac5b9c1fed5dc8133b983899b9ab5f6766f
George Drayson <drayson.g@mac.com>
2019-08-16 14:54:21 +0200

    making homepage responsive

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  var $hamburger = $(".hamburger");
-  $hamburger.hover(function(e) {
-    $hamburger.toggleClass("is-active");
-    // Do something else, like open/close menu
-  });
-});
